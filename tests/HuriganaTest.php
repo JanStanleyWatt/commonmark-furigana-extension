be3581d679b8f5589b299a66990ca3d7349c861a
Jan Stanley Watt <github@mail.whojinn.com>
2023-02-23 03:41:55 +0900

    Add test methods for `HuriganaOpenParser` and more

--- tests/HuriganaTest.php
+++ tests/HuriganaTest.php
@@ -21,11 +21,9 @@ declare(strict_types=1);
 namespace JSW\Tests;
 
 use JSW\Hurigana\HuriganaExtension;
-use JSW\Hurigana\Node\RubyText;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\MarkdownConverter;
-use League\CommonMark\Node\Block\Document;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -46,7 +44,154 @@ final class HuriganaTest extends TestCase
     {
         return '<p>'.$expect.'</p>'."\n";
     }
-    
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateDelimited(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この拡張機能は<ruby>素晴らしい<rt>Wonderful</rt></ruby>');
+        $actual = $converter->convert('この拡張機能は｜素晴らしい《Wonderful》')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateKanji(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この<ruby>拡張機能<rt>かくちょうきのう</rt></ruby>は素晴らしい');
+        $actual = $converter->convert('この拡張機能《かくちょうきのう》は素晴らしい')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateZenkakuAlphabet(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この拡張機能は<ruby>Ｅｘｃｅｌｌｅｎｔ<rt>すばらしい</rt></ruby>');
+        $actual = $converter->convert('この拡張機能はＥｘｃｅｌｌｅｎｔ《すばらしい》')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateHankakuAlphabet(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この拡張機能は<ruby>Excellent<rt>すばらしい</rt></ruby>');
+        $actual = $converter->convert('この拡張機能はExcellent《すばらしい》')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateZenkakuKatakana(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この拡張機能は<ruby>グレート<rt>Great</rt></ruby>だ');
+        $actual = $converter->convert('この拡張機能はグレート《Great》だ')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateHankakuKatakana(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この拡張機能は<ruby>ｸﾞﾚｰﾄ<rt>Great</rt></ruby>だ');
+        $actual = $converter->convert('この拡張機能はｸﾞﾚｰﾄ《Great》だ')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
+    /**
+     * @covers \JSW\Hurigana\Util\HuriganaKugiri::getKugiri
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::parse
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::getMatchDefinition
+     * @covers \JSW\Hurigana\Parser\HuriganaOpenParser::__construct
+     */
+    public function testRubySeparateHiragana(): void
+    {
+        $environment = new Environment($this::DEFAULT_RULE);
+
+        $environment->addExtension(new CommonMarkCoreExtension())
+                    ->addExtension(new HuriganaExtension());
+
+        $converter = new MarkdownConverter($environment);
+
+        $expect = $this->makeExpect('この拡張機能、<ruby>いいかんじ<rt>some good</rt></ruby>だ');
+        $actual = $converter->convert('この拡張機能、いいかんじ《some good》だ')->getContent();
+
+        $this->assertSame($expect, $actual);
+    }
+
     /**
      * @covers ::useRPTag
      */
@@ -68,5 +213,4 @@ final class HuriganaTest extends TestCase
 
         $this->assertSame($expect, $actual);
     }
-
 }
